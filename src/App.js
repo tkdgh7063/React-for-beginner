65328e2fcd5774e6e0ad4b1e2c40dea1162a4d31
tkdgh7063 <hoo412298@gmail.com>
2023-01-15 00:41:08 +0900

    Update App.js
    used useEffect with deps and cleanup function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  /*
+  function effectFn() {
+    console.log("created :)");
+    return destroyFn;
+  }
+  function destroyFn() {
+    // cleanup function
+    console.log("destroyed :(");
+  }
+  */
+  useEffect(() => {
+    console.log("created :)");
+    return () => {
+      // cleanup function
+      console.log("destroyed :(");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
+    </div>
+  );
+
+  /*
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   useEffect(() => {
     console.log("CALL THE API....");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
+
   return (
     <div>
+      <input
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+        value={keyword}
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
   );
+  */
 }
 
 export default App;
