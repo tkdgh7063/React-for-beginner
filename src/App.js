c1ee31b06d2ec24621ae339d41ff6262d6724cae
tkdgh7063 <hoo412298@gmail.com>
2023-01-17 21:33:32 +0900

    Revert "Update App.js"
    This reverts commit c9525ad2eb0b24a2444e776d7d8a3214cb9198e5.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import { useState, useEffect } from "react";
+import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  console.log("i run all the time");
-  useEffect(() => {
-    console.log("CALL THE API....");
-  }, []);
   return (
-    <div>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+    <div className="App">
+      <h1 className={styles.title}>Welcome Back!!</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
