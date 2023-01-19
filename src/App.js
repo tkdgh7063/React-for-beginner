a904cecdb4e9e89e77920d7d8335cc69a3faa9e2
tkdgh7063 <hoo412298@gmail.com>
2023-01-19 18:53:07 +0900

    Added movie info catalog

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum-rating=8.5&limit=50`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
   return (
-    <div className="App">
-      <h1>Welcome Back!!</h1>
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>
+                {movie.title}({movie.year})
+              </h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g, index) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
