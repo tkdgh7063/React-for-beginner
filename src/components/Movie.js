1102573077a0db7d80d31f809855cb20d931c5da
tkdgh7063 <hoo412298@gmail.com>
2023-01-21 22:13:55 +0900

    Webification of Movie App

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,18 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-function Movie({ coverImg, title, year, summary, genres }) {
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        {title}({year})
+        <Link to={`/movie/${id}`}>
+          {title}({year})
+        </Link>
       </h2>
       <p>{summary}</p>
       <ul>
-        {genres.map((g, index) => (
+        {genres.map((g) => (
           <li key={g}>{g}</li>
         ))}
       </ul>
@@ -18,6 +21,7 @@ function Movie({ coverImg, title, year, summary, genres }) {
 }
 
 Movie.propTypes = {
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   year: PropTypes.number.isRequired,
